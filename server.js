b873c86d114cb4a6b711c801467c2c7d1b5f73b5
cjwebb <colinjameswebb@hotmail.com>
2014-06-30 21:11:44 +0100

    basic registration form

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
     exphbs  = require('express3-handlebars'),
     user_db = require('./user_db.js')
 	app = express();
 
 app.use(express.static(__dirname + '/public'));
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
 	res.render('home', {title: "hello"});
@@ -14,21 +15,24 @@ app.get('/', function(req, res){
 
 app.get('/user/:id', function(req, res){
 	var id = req.params.id;
-
 	user_db.getUser(id, function(err, user){
-		res.render('home', {users: user})
+		res.render('user', {users: user})
 	});
 });
 
 app.get('/register', function(req, res){
+	res.render('register', {title: "user"});
+});
+
+app.post('/register', function(req, res){
 	var user = {
-		name: "Colin",
-	 	email: "colinjameswebb@hotmail.com",
-	 	password: "password1"
+		name: req.body.name,
+		email: req.body.emailAddress,
+		password: req.body.password
 	};
-	user_db.createUser(user, function(err, user){
+	user_db.createUser(user, function(err, userId){
 		if (err) console.log(err);
-		res.render('home', {title: user});
+		res.redirect('/user/' + userId);
 	});
 });
 
